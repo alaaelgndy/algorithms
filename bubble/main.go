52eb8090ec6f12a6cb5ae5b4957a5a4e018f728a
alaaelgndy <alaaelgndy21@gmail.com>
2019-01-27 17:05:45 +0200

    add bubble sort algorithm

---
+++ bubble/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+// insertion sort algorithm
+// best case O(n)
+// worst case O(n^2)
+// average case O(n^2)
+func main() {
+
+	unsorted_slice := slice_generation(10)
+	fmt.Println(unsorted_slice)
+
+	fully_sorted := false
+
+	for !fully_sorted {
+
+		fully_sorted = true
+
+		for i := 1; i < len(unsorted_slice); i++ {
+
+			if unsorted_slice[i-1] > unsorted_slice[i] {
+				unsorted_slice[i-1], unsorted_slice[i] = unsorted_slice[i], unsorted_slice[i-1]
+
+				fully_sorted = false
+			}
+		}
+	}
+
+	fmt.Println(unsorted_slice)
+}
+
+func slice_generation(slice_count int) []int {
+	var new_slice []int
+	for i := 0; i < slice_count; i++ {
+		new_slice = append(new_slice, rand.Intn(100000))
+	}
+	return new_slice
+}
